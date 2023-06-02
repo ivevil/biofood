cc788d48f0d4228819dca5103d591f132d87ca7d
Ivana Andric <ivevil@gmail.com>
2023-06-02 18:01:00 +0200

    Fix padding left for Jumbotron

--- app/components/UI/Jumbotron.tsx
+++ app/components/UI/Jumbotron.tsx
@@ -19,7 +19,7 @@ const Jumbotron: React.FC<JumbotronProps> = (props: JumbotronProps) => {
                 <h1 className="mb-4 text-4xl lg:px-24 font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
                     <div dangerouslySetInnerHTML={{ __html: props.block.title.replace(/(<? *script)/gi, 'illegalscript') }} ></div>
                 </h1>
-                <p className="mb-8 text-lg font-normal text-lime-600 lg:text-xl sm:px-16 lg:px-24">{props.block.description}</p>
+                <p className="mb-8 text-lg font-normal text-lime-600 lg:text-xl lg:px-24">{props.block.description}</p>
                 <div className="flex flex-col space-y-4 lg:px-24 sm:flex-row sm:space-y-0 sm:space-x-4">
                     <a href={props.block.buttonLink} className="inline-flex justify-center hover:text-white items-center py-3 px-5 text-base font-medium text-center border-bio-green bio-gray rounded-lg border hover:bg-lime-600 focus:ring-4 focus:ring-gray-400">
                         {props.block.button}
