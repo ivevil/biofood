48cbe47e38ed0a67e0a3aec3c90037e09ec33645
Ivana Andric <ivevil@gmail.com>
2023-06-05 15:35:13 +0200

    Adjust Blog and blog items

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -7,7 +7,7 @@ interface BlogProps {
         buttonLink: string,
         component: string,
         description: string,
-        details: Array<{ title: string; description: string; image: string; }>
+        details: Array<{ title: string; date:string; description: string; image: string; }>
         id: string,
         image: string,
         sectionName: string,
@@ -21,11 +21,11 @@ const Blog: React.FC<BlogProps> = (props: BlogProps) => {
             <div className="blog bg-center bg-no-repeat bg-[url('/vegetables-twisted.jpg')] bg-blend-multiply bg-cover py-8" style={{ margin: "0 auto" }} id="blog">
                 <Heading textColor="lime" textPosition="right">{props.block.title}</Heading>
                 {/* <div className="grid grid-cols-2 gap-4 align-right" style={{ margin: "0 auto" }}> */}
-                <div className="flex flex-wrap justify-end w-full">
+                <div className="flex flex-wrap justify-center sm:justify-end w-full">
                 <div className="w-3/4 flex flex-end flex-wrap justify-end">
                     {props.block.details.map((detail, index) => {
                         return (
-                            <BlogCard detailImage={detail.image} detailTitle={detail.title} detailDescription={detail.description} key={index} />
+                            <BlogCard detailDate={detail.date} detailImage={detail.image} detailTitle={detail.title} detailDescription={detail.description} key={index} />
                         )
                     })}
                     </div>
--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
+import Link from "next/link";
 
 interface BlogCardProps {
     detailImage: string,
     detailTitle: string,
+    detailDate: string,
     detailDescription: string
 }
 
-const BlogCard: React.FC<BlogCardProps> = ({ detailTitle, detailDescription, detailImage }: BlogCardProps) => {
+const BlogCard: React.FC<BlogCardProps> = ({ detailTitle, detailDate, detailDescription, detailImage }: BlogCardProps) => {
+    const slugTitle = detailTitle.toLowerCase().replace(/\s+/g, '-');
     return (
         <div className="blog-card m-3 bg-white border border-gray-200 rounded-3xl shadow dark:bg-white-800 dark:border-gray-100">
             <a href="#">
                 <Image className="rounded-t-3xl max-h-44 object-cover" src={detailImage} alt="" width="600" height="100" />
             </a>
             <div className="px-6 py-2">
-                <a href="#">
-                    <h5 className="mt-4 mb-2 text-sm font-bold tracking-tight text-gray-900 dark:text-gray-500">{detailTitle}</h5>
-                </a>
+                <Link href={`/blog/${slugTitle}`}>
+                    <h5 className="mt-4 mb-2 text-sm font-bold tracking-tight text-gray-900 dark:text-gray-500">{detailDate}</h5>
+                </Link>
                 <p className="mb-7 font-normal text-sm text-gray-700 dark:text-gray-500">{detailDescription}</p>
             </div>
         </div>
