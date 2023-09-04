0c163a638522a9f7ed47c43116876920a085e537
Ivana Andric <ivevil@gmail.com>
2023-09-04 17:06:11 +0200

    Adjust image on blog post

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -22,28 +22,32 @@ export default function Page({
   if (!currentPost) return <Layout />;
   return (
     <Layout>
-      <main className="pt-20 pb-16 lg:pt-16 lg:pb-24 bg-white antialiased">
-        <div className="flex justify-between px-4 pt-20 mx-auto max-w-screen-xl ">
-          <article className="mx-auto w-full max-w-3xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
-            <header className="mb-4 lg:mb-6 not-format">
-              <address className="flex items-center mb-6 not-italic">
-                <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-                  <img className="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos" />
-                  <div>
-                    <a href="#" rel="author" className="text-xl font-bold text-gray-900">{currentPost.author}</a>
-                    <p className="text-base font-light text-gray-500 dark:text-gray-400">{currentPost.authorRole}</p>
-                    <p className="text-base font-light text-gray-500 dark:text-gray-400"><time title={currentPost.date}>{currentPost.date}</time></p>
+      <main className="pb-16 lg:pb-24 pt-20 bg-white antialiased">
+        <div className="flex justify-between mx-auto">
+          <article className="mx-auto w-full format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+            <img className="mr-4 w-full h-64 object-cover" src={currentPost.image} alt="Jese Leos" />
+            <div className="w-full max-w-3xl mx-auto w-full format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+              <header className="mb-4 lg:mb-6 pt-10 not-format">
+                <address className="flex items-center mb-6 not-italic">
+                  <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                    <img className="mr-4 w-16 h-16 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos" />
+                    <div>
+                      <a href="#" rel="author" className="text-xl font-bold text-gray-900">{currentPost.author}</a>
+                      <p className="text-base font-light text-gray-500 dark:text-gray-400">{currentPost.authorRole}</p>
+                      <p className="text-base font-light text-gray-500 dark:text-gray-400"><time title={currentPost.date}>{currentPost.date}</time></p>
+                    </div>
                   </div>
-                </div>
-              </address>
-              <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">{currentPost.title}</h1>
-            </header>
-            <p className="lead text-black">{currentPost.body}</p>
+                </address>
+                <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl">{currentPost.title}</h1>
+              </header>
+              <p className="lead text-black">{currentPost.body}</p>
+            </div>
+
           </article>
         </div>
       </main>
     </Layout>
-  ) 
+  )
 }
 
 
