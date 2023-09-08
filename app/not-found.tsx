badc80ebc1eff50f4aeb5958aff3faa48954154e
Ivana Andric <ivevil@gmail.com>
2023-09-08 14:28:04 +0200

    Add new article

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,21 +4,21 @@ import Layout from '@/app/components/UI/Layout'
 
 export default function NotFound() {
     return (
-        <section className="bg-white">
-            <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
-                <div>
-                    <p className="text-sm font-medium text-blue-400">404 error</p>
-                    <h1 className="mt-3 text-2xl font-semibold text-gray-800 md:text-3xl">We can’t find that page</h1>
-                    <p className="mt-4 text-gray-500">Sorry, the page you are looking for doesn't exist or has been moved.</p>
+        <Layout>
+            <div className="bg-white">
+                <div className="container flex justify-center items-center flex-col min-h-screen px-10 py-12 mx-auto">
+                        <p className="text-sm font-medium bio-green">404 error</p>
+                        <h1 className="mt-3 text-2xl font-semibold text-gray-800 md:text-3xl">We can’t find that page</h1>
+                        <p className="mt-4 text-gray-500">Sorry, the page you are looking for doesn't exist or has been moved.</p>
 
-                    <div className="flex items-center mt-6 gap-x-3">
+                        <div className="flex items-center mt-6 gap-x-3">
 
-                        <Link href="/" className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-                            Take me home
-                        </Link>
-                    </div>
+                            <Link href="/" className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-bio-green rounded-lg shrink-0 sm:w-auto hover:bg-lime-600">
+                                Take me home
+                            </Link>
+                        </div>
                 </div>
             </div>
-        </section>
+        </Layout>
     )
 }
